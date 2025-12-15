f86349ca232cce461f9c521f75ca77dddef8e6c7
jacumbicos <jcumbicos.r@gmail.com>
2025-12-15 07:25:09 -0300

    Test LLM model

---
+++ src/agent/nodes.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0,
+    max_retries=2
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg)
