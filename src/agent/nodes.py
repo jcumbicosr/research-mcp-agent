6a3ba2a298d741588655f02af8fbe8a732a5b4b9
jacumbicos <jcumbicos.r@gmail.com>
2025-12-15 09:36:26 -0300

    Configure mcp client

--- src/agent/nodes.py
+++ src/agent/nodes.py
@@ -4,6 +4,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel, Field
 from langchain.agents.structured_output import ToolStrategy
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.tools import load_mcp_tools
+
 
 load_dotenv()
 
@@ -13,21 +15,35 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2
 )
 
+client = MultiServerMCPClient(
+    {
+        "research_article": {
+            "transport": "stdio",
+            "command": "python",
+            "args": ["-m", "src.mcp_server.server"],
+        }
+    }
+)
+
 class ClassifierResponse(BaseModel):
     category: str = Field(description="The name of the category.")
 
-def classify_paper():
+async def classify_paper():
     text = "A sport is an activity involving physical exertion and skill in which an individual or team competes against another or others for entertainment."  
     input_message = {"messages": [{"role": "user", "content": text}]}
 
-    agent = create_agent(
-        model=llm,
-        system_prompt="You are a text classification assistant. Classify the following text into one of these categories: 'sports', 'politics', 'technology', 'entertainment'.",
-        response_format=ToolStrategy(ClassifierResponse),
-    )
+    async with client.session("research_article") as session:
+        tools = await load_mcp_tools(session)
 
-    response = agent.invoke(input_message)
-    print(response["structured_response"])
+        agent = create_agent(
+            model=llm,
+            tools=tools,
+            system_prompt="You are a text classification assistant. Classify the following text into one of these categories: 'sports', 'politics', 'technology', 'entertainment'.",
+            response_format=ToolStrategy(ClassifierResponse),
+        )
+
+        response = agent.invoke(input_message)
+        print(response["structured_response"])
 
 if __name__ == "__main__":
     classify_paper()
